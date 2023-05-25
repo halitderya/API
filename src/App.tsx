5b516fe9e59ad576f9101608d1b1d8e23a95a398
Halit Derya <halitderyaa@gmail.com>
2023-05-25 14:51:38 +0100

    child-parent done

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,27 @@
 import React, { useState } from "react";
 import "./App.css";
-import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
-import { useEffect } from "react";
-import RowData from "./components/RowData";
+import RowData, { RowDataProps } from "./components/RowData";
 
-const URL: string = "https://api.publicapis.org/entries";
 
-export interface RowDataProps {
-  API: string;
-  Description: string;
-  Auth: string;
-  HTTPS: boolean;
-  Cors: string;
-  Link: string;
-  Category: string;
-}
 
-interface ApiResponse {
-  count: number;
-  entries: RowDataProps[];
-}
 
 const App: React.FC = () => {
 
-  const [RowData, SetRowData] = useState<RowDataProps[]>([]);
-  const [count, setCount]= useState<number>(0);
-  const [loading, setLoading]= useState<boolean>(true);
+const [selectedApi,setSelectedApi] =useState<RowDataProps |null>(null);
+
+  const handleApiSelect= (api:RowDataProps) => {
+
+    setSelectedApi(api)
+  }
 
 
-  useEffect(() => {
-
-
-fetch(URL)
-.then((response) => response.json())
-.then((data: ApiResponse) =>{
-SetRowData(data.entries);
-setLoading(false);
-
-})
-
-},[]);
 
   return (
 
 <>
-{loading && <p>Loading...</p>}
-
-{!loading &&
-
-(
-  
-<ul>
-  
-  {RowData.map((datas:RowDataProps) =>(
-<>
-    <li key={datas.API}>Name: {datas.Description}      |      Category: {datas.Category}  </li>
-    
-  </>
-    ))}
-</ul>
-)
-
-
-
-}
-
-
+<RowData onSelectApi={handleApiSelect} />
+<h1>test</h1>
 </>
 
 
--- src/components/RowData.tsx
+++ src/components/RowData.tsx
@@ -1,33 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { type } from "os";
 
-type RowDataProps = {
-  count: number;
-  entries: {
-    API: string;
-    Description: string;
-    Auth: string;
-    HTTPS: boolean;
-    Cors: string;
-    Link: string;
-    Category: string;
-  };
-};
+export interface RowDataProps {
+  API: string;
+  Description: string;
+  Auth: string;
+  HTTPS: boolean;
+  Cors: string;
+  Link: string;
+  Category: string;
+}
 
-export const RowData: React.FC<RowDataProps[]> = (props: RowDataProps[]) => {
+interface ApiResponse {
+  count: number;
+  entries: RowDataProps[];
+}
+
+interface ApiListProps {
+  onSelectApi: (api: RowDataProps, id: number) => void;
+}
+
+const URL: string = "https://api.publicapis.org/entries";
+export const RowData: React.FC<ApiListProps> = ({onSelectApi}) => {
+
+
+  const [RowData, SetRowData] = useState<RowDataProps[]>([]);
+  const [count, setCount]= useState<number>(0);
+  const [loading, setLoading]= useState<boolean>(true);
+  
+  useEffect(() => {
+
+
+    fetch(URL)
+    .then((response) => response.json())
+    .then((data: ApiResponse) =>{
+    SetRowData(data.entries);
+    setLoading(false);
+    
+    })
+    
+    },[]);
+    
   return (
-    <tr>
-      <td scope="col">{}</td>
-      {/*       <td scope="col">{props.entries.Auth}</td>
-      <td scope="col">{props.entries.Category}</td>
-      <td scope="col">{props.entries.Description}</td>
-      <td scope="col">{props.entries.HTTPS}</td>
-      <td scope="col">{props.entries.Link}</td> */}
-      <td>
-        <button className="btn btn-danger"> Delete</button>
-      </td>
-    </tr>
+   
+    <>
+{loading && <p>Loading...</p>}
+
+{!loading &&
+
+(
+  
+<ul>
+  
+  {RowData.map((datas:RowDataProps) =>(
+<>
+    <li key={datas.API}>Name: {datas.Description} Category: {datas.Category}  </li>
+    
+  </>
+    ))}
+</ul>
+)
+
+
+
+}
+
+
+</>
+
+
   );
 };
 
